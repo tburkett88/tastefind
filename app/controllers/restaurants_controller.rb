3129f89dbfdd78e637f38cc12b7d7acb805ace81
tburkett88 <airburkett@AirBurketts-MacBook-Air.local>
2016-05-09 18:30:19 -0500

    first draft

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,15 @@
+class RestaurantsController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+  end
+
+  def signup
+  end
+
+  def restaurant
+  end
+
+end
